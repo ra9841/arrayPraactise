07c14b88b8810487a304c038020a9a573dcab837
Rabin <rabin.shrestha7@gmail.com>
2023-08-13 10:33:02 -0700

    array practise

---
+++ src/practise1/ArrayPractise4.java
@@ -0,0 +1,16 @@
+package practise1;
+
+public class ArrayPractise4 {
+
+	public static void main(String[] args) {
+		String[] str= {"rabin"};
+		System.out.println(str.length);
+		
+		for(int i=0;i<str.length;i++) {
+			System.out.println(str[i]);
+		}
+		//i<=str.length  //ArrayIndexOutOfBoundsException
+
+	}
+
+}
---
+++ src/practise1/TwoDimensionalArray.java
@@ -0,0 +1,14 @@
+package practise1;
+//Arrays of arrays for multidimensional array creation.Advantage of this approach:Memeory utilization will improved.
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		int[][] x=new int[2][];//two dimentional
+		x[0]=new int[2];
+		x[1]=new int[3];
+		System.out.println(x);
+		
+		
+	}
+
+}
